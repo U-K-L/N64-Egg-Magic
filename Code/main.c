835a07cc4b7553eb295a070428ac5c3df4a488cc
U.K.L <WilliamsNoah1720@gmail.com>
2021-12-21 13:53:14 -0500

    commit message

---
+++ Code/main.c
@@ -0,0 +1,13 @@
+/*
+  Creator: U.K.L.
+  Starts the main process to begin the game.
+*/
+
+#include <nusys.h>
+
+void mainproc(void *dummy)
+{
+  
+  nuGfxInit();//Initializes the graphics thread.
+  while(1);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# N64-Egg-Magic
+An N64 Homebrew Game about eggs and magic. 
